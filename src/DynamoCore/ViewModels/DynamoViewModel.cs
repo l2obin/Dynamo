198d34d513beab0a31ed488593919eb145817e6d
l2obin <robin.ang.wee.kiat@autodesk.com>
2013-09-17 17:55:58 +0800

    Implemented Fit to view function and added UI at top right
    Added Shortcut:
       Ctrl + 0 : Zoom and fit all node in the view
       Ctrl + 0 again : Toggle between Auto Zoom Fit view and 100% Zoom

--- src/DynamoCore/ViewModels/DynamoViewModel.cs
+++ src/DynamoCore/ViewModels/DynamoViewModel.cs
@@ -160,6 +160,7 @@ namespace Dynamo.ViewModels
         public DelegateCommand PanCommand { get; set; }
         public DelegateCommand ZoomInCommand { get; set; }
         public DelegateCommand ZoomOutCommand { get; set; }
+        public DelegateCommand FitViewCommand { get; set; }
 
         /// <summary>
         /// An observable collection of workspace view models which tracks the model
@@ -437,6 +438,7 @@ namespace Dynamo.ViewModels
             PanCommand = new DelegateCommand(Pan, CanPan);
             ZoomInCommand = new DelegateCommand(ZoomIn, CanZoomIn);
             ZoomOutCommand = new DelegateCommand(ZoomOut, CanZoomOut);
+            FitViewCommand = new DelegateCommand(FitView, CanFitView);
 
             DynamoLogger.Instance.PropertyChanged += new System.ComponentModel.PropertyChangedEventHandler(Instance_PropertyChanged);
 
@@ -1160,6 +1162,16 @@ namespace Dynamo.ViewModels
         {
             return CurrentSpaceViewModel.ZoomOutCommand.CanExecute(parameter);
         }
+
+        public void FitView(object parameter)
+        {
+            CurrentSpaceViewModel.FitViewCommand.Execute(parameter);
+        }
+
+        internal bool CanFitView(object parameter)
+        {
+            return CurrentSpaceViewModel.FitViewCommand.CanExecute(parameter);
+        }
     }
 
     public class ZoomEventArgs : EventArgs
@@ -1167,12 +1179,17 @@ namespace Dynamo.ViewModels
         internal enum ZoomModes
         {
             ByPoint = 0x00000001,
-            ByFactor = 0x00000002
+            ByFactor = 0x00000002,
+            ByFitView = 0x00000004
         }
 
         internal Point Point { get; set; }
         internal double Zoom { get; set; }
         internal ZoomModes Modes { get; private set; }
+
+        internal Point Offset { get; set; }
+        internal double FocusWidth { get; set; }
+        internal double FocusHeight { get; set; }
 
         internal ZoomEventArgs(double zoom)
         {
@@ -1193,12 +1210,29 @@ namespace Dynamo.ViewModels
             this.Modes = ZoomModes.ByPoint | ZoomModes.ByFactor;
         }
 
-        bool hasPoint()
+        internal ZoomEventArgs(Point offset, double focusWidth, double focusHeight)
+        {
+            this.Offset = offset;
+            this.FocusWidth = focusWidth;
+            this.FocusHeight = focusHeight;
+            this.Modes = ZoomModes.ByFitView;
+        }
+
+        internal ZoomEventArgs(Point offset, double focusWidth, double focusHeight, double zoom)
+        {
+            this.Offset = offset;
+            this.FocusWidth = focusWidth;
+            this.FocusHeight = focusHeight;
+            this.Zoom = zoom;
+            this.Modes = ZoomModes.ByFitView | ZoomModes.ByFactor;
+        }
+
+        internal bool hasPoint()
         {
             return this.Modes.HasFlag(ZoomModes.ByPoint);
         }
 
-        bool hasZoom()
+        internal bool hasZoom()
         {
             return this.Modes.HasFlag(ZoomModes.ByFactor);
         }
